c8d6b6ea4896647e2f97b6e2a52d9a99a76bb80a
Ehrenmann34 <mattia.erne@gmx.ch>
2022-12-30 15:45:05 +0100

    idek

--- Up-Down-RPG/Assets/Scripts/PlayerMovement.cs
+++ Up-Down-RPG/Assets/Scripts/PlayerMovement.cs
@@ -26,9 +26,9 @@ public class PlayerMovement : MonoBehaviour
 
         //Flip player when moving left-right
         if (horizontalInput> 0.01f)
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(1.5f, 1.5f, 1);
         else if (horizontalInput< -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-1.5f, 1.5f, 1);
 
         body.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
     }
